8bd357cc510b2754327de223892e1c4233f7c110
Dexeat <Dexeat@users.noreply.github.com>
2020-02-03 09:40:50 +0100

    Deplacement avec num

--- explo.c
+++ explo.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
+struct Lieux
+{
+    char nom[50];
+    char desc[50];
+    int diff;
+    int link[5];
+};
+
+typedef struct Lieux lieux;
+
+int deplacement(int pos,lieux liste[]){
+    int choix;
+    printf("Choisisez votre lieux:\n");
+    scanf("%d",&choix);
+    printf("Vous vous deplacez de %s a %s\n\n",liste[pos].nom,liste[choix].nom);
+
+    return(choix);
+}
+
+
+
 int main(){
+    
+    lieux Route_de_depart = {"Route de depart","Le lieux ou vous debutez",0,{1,2}};
+    lieux VillageA = {"VillageA","Premier village que vous croisez!",1,{0,2,3}};
+    lieux VillageB = {"VillageB","Un peu plus grand que le VillageA",1,{0,1,3}};
+
+    lieux liste_lieux[3] = {Route_de_depart,VillageA,VillageB};
+
+    int pos = 0;
+
+    while (1)
+    {
+        pos = deplacement(pos,liste_lieux);
+    }
+    
 
     return 0;
 }
